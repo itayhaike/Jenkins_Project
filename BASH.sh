1ab34d114773afdf7ee04500b3f85417d54cbd49
itayhaike <81685066+itayhaike@users.noreply.github.com>
2022-08-03 17:29:16 +0300

    Update BASH.sh

--- BASH.sh
+++ BASH.sh
@@ -1,28 +1,2 @@
 #!bin/bash
-for i in {1..100..3}
-do
-       echo $i
-done
-
-
-#x=1
-#while [ $x -le 100 ]; do
- # echo $x
-  #x=$(( $x +3 ))
-#done
-
-
-#i=1
-#until [ $i -ge 100 ]
-#do
-#       echo $i
-#       ((i=i+3))
-#done
-
-while 1=1; do
-x=1
-echo $((x+3))
-echo $((x*100))
-
-done    
-
+echo -e "I AM A BASH SCRIPT :))))\nHELLO DIMA"
